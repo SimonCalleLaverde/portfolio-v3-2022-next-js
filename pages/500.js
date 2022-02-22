15a48c180754c53b536df342fcd8f6c1895f24a5
Simon Calle Laverde <kyverde88@gmail.com>
2022-02-22 17:47:12 -0500

    Organizing Styles. Organizing Files Structure.
    - Remembering/Going through the prev "React.js/Next.js" stuff I had built. To start:
    º Reviewing 'styles' from "todo-list-react".
    º Reviewing 'router' structure in "react-router", "next-js-blog" (posts), "next-js-in-100-seconds-fireship" (cars), & "business-web-v3-next-js" (news).
    º Reviewing specifically 'pages' structure in "next-js-blog".
    º Reviewing 'components/layout.js' + its 'styles', in "next-js-blog".
    º Looking at 'posts' & 'public' folders in "next-js-blog".
    
    - Removing 'vercel' old image.
    - 'favicon.png' back to 'favicon.ico'.
    - Adding 'public/images' folder with example image for now.
    - Creating 'components' folder in route.
    - '_app.js' small edits.
    - Adding/reviewing '404', '500' pages from "next-js-blog".
    
    -- Commit before a late lunch.

---
+++ pages/500.js
@@ -0,0 +1,6 @@
+// 500 (Custom) Page // Custom error page // File statically generated at build time
+export default function Custom500() {
+  return <h1>500 - Server-side error occurred</h1>
+};
+
+// Note: You can use getStaticProps inside this page if you need to fetch data at build time
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
+// Imports
 import "../styles/globals.scss"
 
+// Adding Global CSS (Can Only Within This "_app.js")
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps}/>
 }
