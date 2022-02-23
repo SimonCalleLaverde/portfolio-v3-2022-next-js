2fee37bf441abfa90d39fa39735f4bd4ec510e0a
Simon Calle Laverde <kyverde88@gmail.com>
2022-02-22 23:30:30 -0500

    Organizing Styles. Organizing Files Structure. (Learning Process As Well).
    - Removing 'index_(old).js'.
    - Basing `routes` and 'pages' structure a bit to my old "business-web-v3-next-js" & "next-js-in-100-seconds-fireship" examples to start.
    - Creating same temporary ".json" files within 'public' directory to simulate a API/Data-Base/CMS (for projects soon).
    - Removing '404 (Custom) Page', I prefer "Next.js" default's look for now. '500 (Custom) Page' I left the custom.
    
    -- "News" shows both 'link' methods.

--- pages/500.js
+++ pages/500.js
@@ -1,4 +1,5 @@
 // 500 (Custom) Page // Custom error page // File statically generated at build time
+// Same exacts was for '404 (Custom) Page', with "return <h1>404 - Page Not Found :(</h1>" but I left Next's default for now
 export default function Custom500() {
   return <h1>500 - Server-side error occurred</h1>
 };
