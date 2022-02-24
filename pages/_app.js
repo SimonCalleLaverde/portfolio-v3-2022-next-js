2b61f4581a5b5d76dd0803954630d776e6939c1d
Simon Calle Laverde <kyverde88@gmail.com>
2022-02-23 21:48:00 -0500

    Organizing Styles. Head.
    - As per "todo-list-react".
    - Removing "globals.scss".
    - Adding "mixins.scss" & "fonts.scss" + "fonts" folder with the '@font-face' fonts.
    - Adding "theme.scss" & "tablet_and_desktop.scss".
    
    - <Head/> component based in my previous folio made in "Jekyll".
    º Revising all meta-tags.
    º Re-doing keywords.
    º Added "Saol Display Regular" font through </Head>.
    
    -- Need to see how </Head> component works for the other pages, to have one shared snippet.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 // Imports
-import "../styles/globals.scss"
+//import "../styles/mixins.scss"
+//import "../styles/fonts.scss"
+import "../styles/theme.scss"
+import "../styles/tablet_and_desktop.scss"
 
 // Adding "Globals" CSS (Can Only Within This '_app.js')
 export default function MyApp({ Component, pageProps }) {
