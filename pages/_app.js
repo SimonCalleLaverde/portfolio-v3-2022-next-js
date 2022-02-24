aafde1a5989c1586de78383c59e8febb9acb76e8
Simon Calle Laverde <kyverde88@gmail.com>
2022-02-23 22:04:33 -0500

    Changed Styles File Structure Using One Main File.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 // Imports
 //import "../styles/mixins.scss"
 //import "../styles/fonts.scss"
-import "../styles/theme.scss"
-import "../styles/tablet_and_desktop.scss"
+//import "../styles/theme.scss"
+//import "../styles/tablet_and_desktop.scss"
+import "../styles/main.scss"
 
 // Adding "Globals" CSS (Can Only Within This '_app.js')
 export default function MyApp({ Component, pageProps }) {
