bbc25cbab29192d24a989bd897f1618eddde3c5d
Simon Calle Laverde <kyverde88@gmail.com>
2022-02-28 23:29:21 -0500

    Organizing Styles.
    -- Really changes from Thursday. Friday had lunch invitation and couldn't continue on this till now.

--- pages/index.js
+++ pages/index.js
@@ -65,28 +65,22 @@ export default function HomePage() {
 
         <div className={styles.grid}>
           <a href="/" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>Documentation</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="/" className={styles.card}>
-            <h2>Learn &rarr;</h2>
+            <h2>Learn</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="/"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
+          <a href="/" className={styles.card}>
+            <h2>Examples</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href="/"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          <a href="/" className={styles.card}>
+            <h2>Deploy</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
